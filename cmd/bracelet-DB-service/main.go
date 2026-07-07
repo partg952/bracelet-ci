4ed9a12d9bf8aa0a40b4566b2599d506c8ba24ad
Parth sharma <dankparth@gmail.com>
2026-07-08 03:34:26 +0530

    Basic SSE Complete , but switch from Job Level streaming to Project level streaming

--- cmd/bracelet-DB-service/main.go
+++ cmd/bracelet-DB-service/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	dbpkg "bracelet-cicd/internal/bracelet-DB-service/db"
 	_ "bracelet-cicd/internal/bracelet-DB-service/db-actions/entity-actions"
+	"bracelet-cicd/internal/bracelet-DB-service/models"
 	"bracelet-cicd/internal/bracelet-DB-service/parser"
+	"bracelet-cicd/internal/bracelet-DB-service/util"
 	"encoding/json"
 	"io"
 	"log"
@@ -14,16 +16,41 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func init() {
 	_ = godotenv.Load("../../.env")
+}
 
+func main() {
 	dbInstance, err := dbpkg.New(os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("Database connection failed: %v", err)
 	}
 	defer dbInstance.Conn.Close()
-
+	broker := util.NewHub()
 	r := gin.Default()
+	r.GET("/stream", func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "text/event-stream")
+		c.Writer.Header().Set("Cache-Control", "no-cache")
+		c.Writer.Header().Set("Connection", "keep-alive")
+		jobId := c.Query("job_id")
+		if jobId == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "job_id is required"})
+			return
+		}
+
+		channel, unsubscribe := broker.Subscribe(jobId)
+		defer unsubscribe()
+
+		c.Stream(func(w io.Writer) bool {
+			select {
+			case <-c.Request.Context().Done():
+				return false
+			case t := <-channel:
+				c.SSEvent("database-event", t)
+				return true
+			}
+		})
+	})
 	r.POST("/event", func(ctx *gin.Context) {
 		body, err := io.ReadAll(ctx.Request.Body)
 		if err != nil {
@@ -50,6 +77,9 @@ func main() {
 			log.Printf("[Event Execution error] %v", err)
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
 			return
+		}
+		if parsedEvent.EntityName == "job" {
+			broker.Publish(parsedEvent.EntityData.(models.Job).JobId, result)
 		}
 
 		ctx.JSON(http.StatusOK, result)
